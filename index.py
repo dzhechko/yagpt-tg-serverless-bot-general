5165d8962bd74b7ae3870d41b406c95e421d7052
Dmitry Zhechkov <dzhechkov@yandex-team.ru>
2024-03-21 09:44:35 +0300

    first commit

---
+++ index.py
@@ -0,0 +1,245 @@
+import asyncio
+import json
+import os
+
+#Библиотеки для работы с телеграмм
+from telegram import Update, Message, Chat
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
+
+#Библиотеки для YaGPT и Langchain
+from yandex_chain import YandexLLM
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+# Default settings
+default_temperature = 0.6
+default_context_window = 8000
+model_list = [
+    "YandexGPT Lite",
+    "YandexGPT"      
+]    
+default_model_index = 1 #используем большую модель YandexGPT
+
+# Current settings
+current_temperature = default_temperature
+current_context_window = default_context_window
+current_model_index = default_model_index
+
+HELP_MESSAGE = """Команды:
+⚪ /new – Начать новый диалог
+⚪ /mode – Выбрать модель
+⚪ /settings – Показать и изменить настройки
+⚪ /history – Показать историю беседы
+⚪ /help – Показать подсказку
+
+"""
+
+SETTINGS_MESSAGE = """Cуществующие настройки:
+⚪ Степень креативности /temperature = {current_temperature}. 
+    Допустимый интервал: от 0 до 1
+⚪ Размер контектного окна /context = {current_context_window}. 
+    Допустимый интервал: от 1000 до 8000
+"""
+
+ABOUT_MESSAGE = """
+
+Я бот-помощник, который построен на базе YandexGPT.
+Моя основная задача - отвечать на все твои вопросы, и помогать в решении различных задач.
+
+Типовые сценарии работы со мной - это узнавать разные факты или решать креативные задачи :)
+Постараюсь быть максимально полезным!
+
+Ты можешь выбрать модель YaGPT, которую хочешь использовать (https://cloud.yandex.ru/ru/docs/yandexgpt/concepts/models).
+Можно настроить размер ее контекстного окна и параметры креативности. 
+"""
+
+
+async def new_dialog_handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    reply_text = "Контекст предыдущей беседы удален. Можете продолжить общение."
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=reply_text)
+
+async def show_chat_modes_handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # await register_user_if_not_exists(update, context, update.message.from_user)
+    # if await is_previous_message_not_answered_yet(update, context): return
+
+    # user_id = update.message.from_user.id
+    # db.set_user_attribute(user_id, "last_interaction", datetime.now())
+    global model_list, current_model_index
+    current_model = model_list[current_model_index]
+    message_local = """
+    Используется модель {model} (https://cloud.yandex.ru/ru/docs/yandexgpt/concepts/models)
+    Чтобы поменять модель используй команды:
+    /model = lite     для YandexGPT lite
+    /model = pro      для YandexGPT
+    """
+    formatted_message = message_local.format(model=current_model)
+    reply_text = formatted_message   
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=reply_text)
+    # await update.message.reply_text(text, reply_markup=reply_markup, parse_mode=ParseMode.HTML)
+
+async def show_settings_handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global current_temperature, current_context_window    
+    formatted_message = SETTINGS_MESSAGE.format(current_temperature=current_temperature, current_context_window=current_context_window)
+    reply_text = formatted_message    
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=reply_text)
+
+async def show_history_handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    reply_text = "История беседы"
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=reply_text)
+
+async def show_help_handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    reply_text = ""
+    reply_text += ABOUT_MESSAGE
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=reply_text)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    reply_text = """
+    Привет!
+    Я бот-помощник, который построен на базе YandexGPT.
+    Моя основная задача - помочь тебе с ответами на вопросы и с решением креативных задач 
+    (но не забудь выставить нужную степень моей креативности!)
+    🤖\n
+    """
+    reply_text += HELP_MESSAGE
+
+    # await update.message.reply_text(reply_text, parse_mode=ParseMode.HTML)
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=reply_text)
+    # await show_chat_modes_handle(update, context)
+
+async def set_temperature_handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global current_temperature
+    message_text = update.message.text
+    if message_text.startswith('/temperature = '):
+        value = message_text.split('=')[-1].strip()
+    elif message_text.startswith('/temperature '):
+        value = message_text.split(' ')[-1].strip()
+    temperature = float(value)
+    if 0 <= temperature <= 1:
+        current_temperature = temperature
+    message_local = """
+    Текущее значение температуры теперь = {current_temperature}
+    """
+    formatted_message = message_local.format(current_temperature=current_temperature)
+    reply_text = formatted_message    
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=reply_text)
+
+async def set_context_handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global current_context_window
+    message_text = update.message.text
+    if message_text.startswith('/context = '):
+        value = message_text.split('=')[-1].strip()
+    elif message_text.startswith('/context '):
+        value = message_text.split(' ')[-1].strip()
+    context = int(value)
+    if 1000 <= context <= 8000:
+        current_context_window = context
+    message_local = """
+    Текущее значение размера контекстного окна теперь = {current_context} токенов.
+    """
+    formatted_message = message_local.format(current_context=current_context_window)
+    reply_text = formatted_message    
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=reply_text)
+
+async def set_model_handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global model_list, current_model_index
+
+    message_text = update.message.text
+    if '/model = lite' in message_text or '/model lite' in message_text:
+        current_model_index = 0
+    elif '/model = pro' in message_text or '/model pro' in message_text:
+        current_model_index = 1
+    
+    current_model = model_list[current_model_index]
+    message_local = """
+    Текущая LLM-модель, которую используем: {current_model}.
+    """
+    formatted_message = message_local.format(current_model=current_model)
+    reply_text = formatted_message    
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=reply_text)
+
+async def process_text_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global current_temperature, current_context_window, current_model_index
+    # вопрос пользователя
+    message_text = update.message.text
+
+    if  message_text == "/start":
+        # await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+        await start(update, context)
+    elif message_text == "/new":
+        await new_dialog_handle(update, context)
+    elif message_text == "/mode":
+        await show_chat_modes_handle(update, context)
+    elif message_text == "/settings":
+        await show_settings_handle(update, context)
+    elif message_text == "/history":
+        await show_history_handle(update, context)
+    elif message_text == "/help":
+        await show_help_handle(update, context)
+    elif message_text.startswith("/temperature"):
+        await set_temperature_handle(update, context)
+    elif message_text.startswith("/context"):
+        await set_context_handle(update, context)
+    elif message_text.startswith("/model"):
+        await set_model_handle(update, context)
+    else:
+        # обращение к модели YaGPT
+        yagpt_folder_id = os.environ["YAGPT_FOLDER_ID"]
+        yagpt_api_key = os.environ["YAGPT_API_KEY"]
+        yagpt_temperature = current_temperature
+        yagpt_max_tokens = current_context_window
+
+        # if current_model_index==0: 
+            # model_uri = "gpt://"+str(yagpt_folder_id)+"/yandexgpt-lite/latest"
+        # else:
+            # model_uri = "gpt://"+str(yagpt_folder_id)+"/yandexgpt/latest"    
+        # model = ChatYandexGPT(api_key=yagpt_api_key, model_uri=model_uri, temperature = yagpt_temperature)
+
+        yagpt_prompt = """
+        Отвечай на вопросы коротко и точно. Следуй пожеланиям пользователя, до тех пор пока они не выходят за рамки этики. Если не знаешь ответ, вежливо напиши об этом.
+        """
+        if current_model_index == 1: bool_lite = False #используем pro модель YandexGPT
+        if current_model_index == 0: bool_lite = True  #используем lite модель YandexGPT
+        llm = YandexLLM(api_key=yagpt_api_key, folder_id=yagpt_folder_id, temperature = yagpt_temperature, max_tokens=yagpt_max_tokens, use_lite=bool_lite)        
+        str2 = """Вопрос: {question}
+        Твой ответ:"
+        """
+        template = f"{yagpt_prompt} {str2}"
+        prompt = PromptTemplate(template=template, input_variables=["question"])
+        llm_chain = LLMChain(prompt=prompt, llm=llm)
+        response = llm_chain.run({"question": message_text})
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+
+
+bot = ApplicationBuilder().token(os.environ["BOT_TOKEN"]).updater(None).build()
+
+bot.add_handler(CommandHandler("start", start))
+bot.add_handler(CommandHandler("mode", show_chat_modes_handle))
+
+bot.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), process_text_message))
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(bot.initialize())
+
+async def handler(event, context):
+    message = json.loads(event["messages"][0]["details"]["message"]["body"])
+    update = Update(
+        update_id=message["update_id"],
+        message=Message(
+            message_id=message["message"]["message_id"],
+            date=message["message"]["date"],
+            chat=Chat(
+                id=message["message"]["chat"]["id"],
+                type=message["message"]["chat"]["type"]
+            ),
+            text=message["message"]["text"],
+        )
+    )
+
+    await bot.process_update(update)
+
+    return {
+        "statusCode": 500,
+    }
---
+++ readme.md
@@ -0,0 +1,94 @@
+### Краткая информация
+Telegram-бот на serverless-стеке Yandex Cloud, который использует YandexGPT при ответе на вопросы прользователей. 
+Реализован с помощью библиотеки [python-telegram-bot](https://docs.python-telegram-bot.org/en/stable/index.html) на Python.
+Данная версия бота не помнит предыдущие сообщения, поэтому решение задачи проводится в рамках текущего вопроса.
+
+Настройки telegram-бота можно менять по своему усмотрению.
+
+В стоимость ресурсов для приложения входят:
+- Плата за "общение" с моделью YandexGPT (см. [тарифы YandexGPT](https://cloud.yandex.ru/ru/docs/yandexgpt/pricing))
+- Плата за количество вызовов функции, вычислительные ресурсы, выделенные для выполнения функции, и исходящий трафик (см. [тарифы Yandex Cloud Functions](https://cloud.yandex.ru/ru/docs/functions/pricing)).
+- Плата за количество запросов к API-шлюзу и исходящий трафик (см. [тарифы Yandex API Gateway](https://cloud.yandex.ru/ru/docs/api-gateway/pricing)).
+- Плата за количество запросов к стандартной очереди (см. [тарифы Yandex Message Queue](https://cloud.yandex.ru/ru/docs/message-queue/pricing)).
+- Плата за хранение и запрос секретов (см. [тарифы Yandex Lockbox](https://cloud.yandex.ru/ru/docs/lockbox/pricing)).
+
+### Инструкция по развертыванию
+1. Зарегистрируйте вашего бота в Telegram и получите токен:
+
+1.1. Запустите бота BotFather и отправьте ему команду `/newbot`
+1.2. Укажите имя вашего бота, например Serverless Echo Telegram Bot. Это имя будут видеть пользователи при общении с ботом.
+1.3. Укажите имя пользователя вашего бота, например ServerlessHelloTelegramBot. По имени пользователя можно будет найти бота в Telegram. Имя пользователя должно оканчиваться на ...Bot или ..._bot.
+На экране появится токен Telegram-бота.
+
+2. [Создайте](https://cloud.yandex.ru/docs/lockbox/operations/secret-create) секрет Yandex Lockbox. Нужен секрет для телеграмм-бота и для YandexGPT. В поле Ключ секрета для телеграмм-бота укажите `TG_TOKEN`, в поле Значение — полученный токен Telegram-бота. Добавьте еще 2 пары Key-Value для YandexGPT, необходимо завести два Ключа и указать их значения: `YAGPT_FOLDER_ID` и `YAGPT_API_KEY`. Для получения `YAGPT_API_KEY` необходимо создать [сервисный аккаунт](https://cloud.yandex.ru/ru/docs/iam/quickstart-sa), которому должны быть назначены роли ai.languageModels.user для доступа к модели YandexGPT.
+
+3. В [консоли](https://console.cloud.yandex.com/) управления выберите каталог, в котором хотите развернуть приложение.
+
+4. Выберите сервис Cloud Apps.
+
+5. На панели слева выберите Магазин приложений.
+
+6. Выберите Demo Telegram Bot YandexGPT (или Demo Telegram Bot, но тогда придется добавить в проект все исходные файлы из данного репозитория и поменять среду исполнения на Python) и нажмите кнопку Использовать.
+
+7. Укажите:
+
+- Имя приложения.
+- (Опционально) Описание приложения.
+- Сервисный аккаунт с ролью admin на каталог или выберите Автоматически, чтобы нужный сервисный аккаунт создался при установке приложения. От имени этого сервисного аккаунта будут создаваться ресурсы приложения.
+- Идентификатор секрета Yandex Lockbox, который создали ранее.
+
+8. Нажмите кнопку Установить и дождитесь, пока приложение установится.
+
+9. На странице Обзор, в разделе Ресурсы приложения, найдите API-шлюз, перейдите на его страницу и скопируйте ссылку на служебный домен.
+
+10. Чтобы настроить связь между функцией и Telegram-ботом, выполните запрос. Вместо <токен бота> укажите токен Telegram-бота, вместо <домен API-шлюза> — ссылку на служебный домен API-шлюза.
+- Для Linux/macOS
+```
+curl \
+  --request POST \
+  --url https://api.telegram.org/bot<токен бота>/setWebhook?url=https://<Домен API-шлюза>/echo
+```
+- Для Windows (cmd)
+```
+curl ^
+  --request POST ^
+  --url "https://api.telegram.org/bot<токен бота>/setWebhook?url=https://<Домен API-шлюза>/echo"
+```
+- Для Windows (Powershell)
+```
+curl.exe `
+  --request POST `
+  --url https://api.telegram.org/bot<токен бота>/setWebhook?url=https://<Домен API-шлюза>/echo
+```
+
+Результат:
+
+{"ok":true,"result":true,"description":"Webhook was set"}
+
+11. Напишите боту в Telegram.
+
+### Полезные ссылки
+[Документация Yandex Cloud Functions](https://cloud.yandex.ru/ru/docs/functions/)
+[Документация Yandex API Gateway](https://cloud.yandex.ru/ru/docs/api-gateway/)
+[Документация Yandex Message Queue](https://cloud.yandex.ru/ru/docs/message-queue/)
+[Документация Yandex Lockbox](https://cloud.yandex.ru/ru/docs/lockbox/)
+[Документация YandexGPT API](https://cloud.yandex.ru/ru/docs/yandexgpt/)
+
+Техническая поддержка
+Служба технической поддержки Yandex Cloud отвечает на запросы 24 часа в сутки, 7 дней в неделю. Доступные виды запросов и срок их обработки зависят от тарифного плана. Подключить платную поддержку можно в [консоли управления](https://support.yandex.cloud/plans?section=plan). Подробнее о порядке оказания [технической поддержки](https://cloud.yandex.ru/ru/docs/support/overview).
+
+### Ресурсы приложения
+| Тип ресурса | Количество |
+| ----------- | ----------- |
+| Сервисные аккаунты	  | 4   |
+| Статический ключ доступа  | 1   |
+| Пользователи каталога | 7   |
+| Message Queue	  | 1   |
+| Бессерверная функция  | 1   |
+| Триггер	   | 1   |
+| API-шлюз  | 1   |
+| YandexGPT API  | 1   |
+
+
+### Лицензионное соглашение
+Используя данный продукт, вы соглашаетесь с [Условиями использования Yandex Cloud Marketplace](https://yandex.ru/legal/cloud_terms_marketplace/)
